f08331d77753c64d525c1680f1c081720303d799
Ben Oeyen <ben.oeyen@gmail.com>
2017-04-22 16:47:10 +0200

    fix function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const admin = require('firebase-admin');
+const functions = require('firebase-functions');
 admin.initializeApp(functions.config().firebase);
 
 exports.validate = functions.https.onRequest((request, response) => {
-  response.send("User", request.query.user, "Guessed", request.query.guess, "in room", request.quesry.room);
+  //firebase.database().ref('room/')
+  response.send("User " + request.query.user + " Guessed " + request.query.guess + " in room " + request.query.room);
 });
 //exports.emptyRoomCleanup = require('./validate-guess');
 
