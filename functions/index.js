1364f8855e65c883a963126bef17809d328ee377
Ben Oeyen <ben.oeyen@gmail.com>
2017-04-22 16:13:35 +0200

    test function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.emptyRoomCleanup = require('./validate-guess');
+exports.validate = functions.https.onRequest((request, response) => {
+  response.send("User", request.query.user, "Guessed", request.query.guess, "in room", request.quesry.room);
+});
+//exports.emptyRoomCleanup = require('./validate-guess');
 
 
