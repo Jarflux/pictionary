33e8205bf42a8736e4e6c53daf569dae0c0eca97
Ben Oeyen <ben.oeyen@gmail.com>
2017-04-22 17:32:27 +0200

    Added database model.

--- functions/validate-guess.js
+++
@@ -1,6 +0,0 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-
-// exports.validate = functions.https.onRequest((request, response) => {
-//   response.send("User",request.query.user,"Guessed", request.query.guess,"in room",request.quesry.room);
-// });
