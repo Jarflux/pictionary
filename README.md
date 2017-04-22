2fe755d7f712a4a6731c016207d7b09a73dcfd76
Mathieu De Haeck <mathieu.de.haeck@gmail.com>
2017-04-22 08:27:54 +0200

    changed name to Pictionary, added material and firebase
    Signed-off-by: Mathieu De Haeck <mathieu.de.haeck@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyNewApp
+# Pictionary
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { MaterialModule } from '../material';
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDlhRyWbIzZmO8LqhqQpAhg8jdBXhoM814',
+  authDomain: 'pictionary-9dca6.firebaseapp.com',
+  databaseURL: 'https://pictionary-9dca6.firebaseio.com',
+  storageBucket: 'pictionary-9dca6.appspot.com',
+  messagingSenderId: '1006398482597'
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +24,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
