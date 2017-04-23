0e6dd9e7248f3903b1c2d7f3faef9d24e1952e94
Pieter-Jan Pauwels <pieterjan.pauwels@asadventure.com>
2017-04-23 14:11:02 +0200

    user script create playerInfo profiles

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -7,70 +7,82 @@ import {FirebaseObjectObservable} from "angularfire2";
 import {isNullOrUndefined} from "util";
 
 @Component({
-  selector: 'app-room',
-  templateUrl: './room.component.html',
-  styleUrls: ['./room.component.scss'],
-  providers: [RoomService]
+    selector: 'app-room',
+    templateUrl: './room.component.html',
+    styleUrls: ['./room.component.scss'],
+    providers: [RoomService]
 })
-export class RoomComponent implements OnInit{
-  private isGuessing: boolean = false;
-  private isArtist: boolean = false;
-  private drawLines: string[];
+export class RoomComponent implements OnInit {
+    private isGuessing: boolean = false;
+    private isArtist: boolean = false;
+    private drawLines: string[];
 
-  private room: Room;
-  private room$: FirebaseObjectObservable<Room>;
+    private roomUid: string;
+    private room: Room;
+    private room$: FirebaseObjectObservable<Room>;
 
-  private guessingWord: string = 'banaan';
-  private endTimeStamp: number;
-  private currentUserId: string = "";
+    private guessingWord: string = 'banaan';
+    private endTimeStamp: number;
+    private currentUserId: string = "";
 
-  constructor(private route: ActivatedRoute,
-              private roomService: RoomService) {
-  }
-
-  ngOnInit() {
-    this.room$ = this.roomService.getRoomById(this.route.snapshot.params['id']);
-    this.room$
-      .subscribe((room: Room) => {
-        this.checkIfRoomStartsGame(room);
-        this.room = room;
-      });
-  }
-
-
-  handleDrawing(drawLines: string[]) {
-    this.roomService.updateDrawings(this.room$, drawLines);
-  }
-
-  handleGuess(guess: string) {
-    console.log('someone guessed', guess);
-  }
-
-  handleTimerRanOut() {
-    console.log('timer has ended');
-  }
-
-  private checkIfRoomStartsGame(newRoom: Room) {
-    this.currentUserId = this.roomService.currentUserId;
-
-    if ((isNullOrUndefined(this.room) || !this.roomService.isRoomInPlayingMode(this.room)) && this.roomService.isRoomInPlayingMode(newRoom)) {
-      this.isGuessing = true;
-      this.isArtist = this.roomService.isCurrentUserTheArtist(newRoom);
-
-      newRoom.startRoundTimestamp = new Date();
-      this.endTimeStamp = this.getEndTimeStamp(newRoom.startRoundTimestamp).getTime();
-    } else {
-      console.log("Nope...");
-      this.isGuessing = false;
-      this.isArtist = false;
-      this.endTimeStamp = undefined;
+    constructor(private route: ActivatedRoute,
+                private roomService: RoomService) {
     }
-  }
 
-  private getEndTimeStamp(startTimestamp: Date) {
-    let endTimestamp = new Date(startTimestamp.getTime());
-    endTimestamp.setMinutes(startTimestamp.getMinutes() + 1);
+    ngOnInit() {
+        this.roomUid = this.route.snapshot.params['id'];
+        this.enterRoom(this.roomUid);
+        this.room$ = this.roomService.getRoomById(this.roomUid);
+        this.room$
+            .subscribe((room: Room) => {
+                this.checkIfRoomStartsGame(room);
+                this.room = room;
+            });
+    }
 
-    return endTimestamp;
-  }
+
+    handleDrawing(drawLines: string[]) {
+        this.roomService.updateDrawings(this.room$, drawLines);
+    }
+
+    handleGuess(guess: string) {
+        console.log('someone guessed', guess);
+        this.roomService.guess(this.roomUid, guess);
+    }
+
+    handleTimerRanOut() {
+        console.log('timer has ended');
+    }
+
+    leaveRoom() {
+        this.roomService.leaveRoom(this.roomUid);
+    }
+
+    private enterRoom(roomUid: string) {
+        this.roomService.enterRoom(roomUid);
+    }
+
+    private checkIfRoomStartsGame(newRoom: Room) {
+        this.currentUserId = this.roomService.currentUserId;
+
+        if ((isNullOrUndefined(this.room) || !this.roomService.isRoomInPlayingMode(this.room)) && this.roomService.isRoomInPlayingMode(newRoom)) {
+            this.isGuessing = true;
+            this.isArtist = this.roomService.isCurrentUserTheArtist(newRoom);
+
+            newRoom.startRoundTimestamp = new Date();
+            this.endTimeStamp = this.getEndTimeStamp(newRoom.startRoundTimestamp).getTime();
+        } else {
+            console.log("Nope...");
+            this.isGuessing = false;
+            this.isArtist = false;
+            this.endTimeStamp = undefined;
+        }
+    }
+
+    private getEndTimeStamp(startTimestamp: Date) {
+        let endTimestamp = new Date(startTimestamp.getTime());
+        endTimestamp.setMinutes(startTimestamp.getMinutes() + 1);
+
+        return endTimestamp;
+    }
 }
