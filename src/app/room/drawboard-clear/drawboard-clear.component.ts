9bf06fff0372bd04940ecc19988801c06cdbe3f7
Thomas Timmers <me@timmersthomas.be>
2017-04-23 16:35:09 +0200

    Drawboard clear - added component

---
+++ src/app/room/drawboard-clear/drawboard-clear.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drawboard-clear',
+  templateUrl: './drawboard-clear.component.html',
+  styleUrls: ['./drawboard-clear.component.scss']
+})
+export class DrawboardClearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
