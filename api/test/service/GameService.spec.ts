1623ae430b51b56c0a1909c2e7ead42deef722f0
Ben Oeyen <ben.oeyen@gmail.com>
2017-05-07 13:34:12 +0200

    Added test framework shizzle

---
+++ api/test/service/GameService.spec.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Ben on 07/05/2017.
+ */
+import {GameService} from "../../src/service/GameService";
+import {expect} from "chai";
+import {} from "jasmine";
+
+describe('GameService', () => {
+
+  it('should always match blaaspijp', () => {
+   // expect(GameService.matchesSingularOrPluralWord("wordDoesntMatter","blaaspijp")).to.be.true;
+  });
+
+});
