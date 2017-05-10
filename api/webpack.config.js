b840129784af35b8ce3ba39cf3c4e1183a92f4a1
Ben Oeyen <ben.oeyen@gmail.com>
2017-05-10 17:19:07 +0200

    Fix testing, building and deploying when running commands withing /api folder ;-)

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,19 +1,64 @@
-module.exports = {
-  entry: './src/_app.ts',
-  output: {
-    filename: 'dist/app.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader'
-      }
-    ]
-  },
-  devServer: { 'hide-modules': true }
+const path = require('path');
+const webpack = require("webpack");
+const nodeExternals = require('webpack-node-externals');
+const BabiliPlugin = require("babili-webpack-plugin");
+
+const babiliOptions = [];
+const productEnv = ["production", "prd", "prod"];
+
+module.exports = function (env) {
+  const webpackPlugins = (productEnv.indexOf(env) > -1) ? [
+      new webpack.LoaderOptionsPlugin({
+        minimize: false,
+        debug: false
+      }),
+      new BabiliPlugin(babiliOptions)
+    ] : [];
+
+  return {
+    entry: './src/_app.ts',
+    output: {
+      libraryTarget: 'commonjs',
+      filename: 'dist/app.js'
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.tsx', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'awesome-typescript-loader'
+            }
+          ]
+        }/*, {
+         test: /\.js$/,
+         exclude: /node_modules/,
+         use: [
+         {
+         loader: 'babel-loader',
+         //options: babelOptions
+         }
+         ]
+         }*/
+      ],
+      loaders: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          loader: 'awesome-typescript-loader'
+        }
+      ]
+    },
+    plugins: webpackPlugins,
+    target: 'node',
+    externals: [nodeExternals()],
+    devtool: 'source-map',
+    devServer: {
+      'hide-modules': true
+    }
+  }
 };
